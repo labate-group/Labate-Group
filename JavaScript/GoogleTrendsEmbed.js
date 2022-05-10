5ecbcc369c9c6ee8619264f222aa0c95396fb7aa
Jake Labate <jake@labate.io>
2022-05-10 14:51:47 -0400

    Create GoogleTrendsEmbed.js

---
+++ JavaScript/GoogleTrendsEmbed.js
@@ -0,0 +1,21 @@
+<script type="text/javascript"
+	src="https://ssl.gstatic.com/trends_nrtr/2884_RC01/embed_loader.js">
+</script>
+<script type="text/javascript">
+	trends.embed.renderExploreWidget
+	("TIMESERIES",
+  	{"comparisonItem":
+	[{
+		"keyword":"Example",
+		"geo":"US",
+		"time":"today 5-y"
+        }],
+        "category":0,
+        "property":""
+    },
+    {
+	"exploreQuery":"date=today%205-y&geo=US&q=Example",
+	"guestPath":"https://trends.google.com:443/trends/embed/"
+    }
+);
+</script>
